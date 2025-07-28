708d779fbcb54d0a2cbd9c7aed984d3c69495713
reclinersofaa <dattatreyathe2912@gmail.com>
2025-07-28 14:17:03 +0530

    another detection change

--- vars/buildMicroservice.groovy
+++ vars/buildMicroservice.groovy
@@ -14,7 +14,7 @@ def call(){
                 steps{
                     script{
                         checkout scm
-                        def serviceName = env.JOB_NAME.replace('-pipeline', '')
+                        def serviceName = env.JOB_NAME
                         env.SERVICE_NAME = serviceName
                         echo "Checking service: ${serviceName}"
                         sh '''
